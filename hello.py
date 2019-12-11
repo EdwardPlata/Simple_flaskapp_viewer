e2eda24693a2a20600c823708cc313a401ab0638
Edward Plata <edward.plata@baruchmail.cuny.edu>
2019-12-11 04:32:59 +0000

    New file updates

--- hello.py
+++ hello.py
@@ -1,13 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
 import os
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return "Hello world"
+def index():
+    return url_for('show_user_profile',username='Edgie')
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    #Show user profile for that user
+    return "User %s" % username
     
+@app.route('/hello')
+def hello_world():
+    return "Hello World"
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return "Post %d" % post_id
+# WHen debugging we can see our errors in the next page
 if __name__=='__main__':
     # We can call from the terminal
     host = os.getenv('ip','0.0.0.0')
     port = int(os.getenv('Port','8080'))
     print(host,port)
+    #app.debug=True
     app.run(host=host,port=port)
---
+++ methods.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template,url_for, request
+import os
+app = Flask(__name__)
+
+@app.route('/login',methods=['GET'])
+def login():
+    # PAss variables using the GET method
+    if request.values:
+        return 'username is '+request.values["username"] # Request values is holding all the inputs
+    else:
+        return'<form method="get" action="/login"><input type = "text" name="username" /><p><button type ="submit">Submit</button></form>'
+if __name__=='__main__':
+    # We can call from the terminal
+    host = os.getenv('ip','0.0.0.0')
+    port = int(os.getenv('Port','8080'))
+    print(host,port)
+    #app.debug=True
+    app.run(host=host,port=port)
